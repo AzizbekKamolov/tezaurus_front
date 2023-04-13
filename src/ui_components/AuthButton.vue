53d3191659f38b7b1e53ce7b86127628b9111f95
AzizbekKamolov <azizbekkamolov19@gmail.com>
2023-04-14 00:34:36 +0500

    disabled:opacity-50 add register button for vuex action settimeouted where desabled button

--- src/ui_components/AuthButton.vue
+++ src/ui_components/AuthButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="w-full bg-indigo-500 text-white text-sm font-bold py-2 px-4 rounded-md hover:bg-indigo-600 transition duration-300 focus:outline-none"
+        class="w-full bg-indigo-500 text-white text-sm font-bold py-2 px-4 rounded-md hover:bg-indigo-600 transition duration-300 disabled:opacity-50"
         type="submit">
     <slot></slot>
     </button>
