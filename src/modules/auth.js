f71cf25ee8f0cd1700a708b0147f3566302ff1e5
AzizbekKamolov <96434885+AzizbekKamolov@users.noreply.github.com>
2023-04-19 17:25:23 +0500

    navigation chack logged in

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -2,6 +2,7 @@ import { setItem } from "../helpers/parsistenceStorage"
 import AuthService from "../service/auth"
 const state = {
     isLoading: false,
+    isLoggedIn:null,
     user:null,
     errors:null
 }
@@ -11,28 +12,34 @@ const mutations = {
         state.isLoading = true
         state.user = null
         state.errors = null
+        state.isLoggedIn = null
     },
     registerSuccess(state, payload) {
         state.isLoading = false
         state.user = payload
+        state.isLoggedIn = true
     },
     registerFailure(state, payload) {
         state.isLoading = false
         state.errors = payload.data.errors
+        state.isLoggedIn = null
     },
 
     loginStart(state) {
         state.isLoading = true
         state.user = null
         state.errors = null
+        state.isLoggedIn = null
     },
     loginSuccess(state, payload) {
         state.isLoading = false
         state.user = payload
+        state.isLoggedIn = true
     },
     loginFailure(state, payload) {
         state.isLoading = false
         state.errors = payload.data.errors
+        state.isLoggedIn = null
     },
 }
 
